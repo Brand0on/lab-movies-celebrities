668aec26b54ae34d26d07a8719532aadf8b2c19e
Brandon Alcalde <brandonalcalde1999@gmail.com>
2022-10-16 14:12:39 +0100

    done

--- index.js
+++ index.js
@@ -1,25 +1,56 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 
-const express = require('express');
-const mongoose = require('mongoose');
-const path = require('path');
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const Celebrity = require("./models/celebrity");
 
 const app = express();
 
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res, next) => {
-  res.render('home');
+app.get("/", (req, res, next) => {
+  res.render("home");
+});
+app.get("/celebrities/create", (req, res, next) => {
+  res.render("celebrities/new-celebrity");
+});
+
+app.get("/celebrities", (req, res, next) => {
+  Celebrity.find({})
+    .then((celebrities) => {
+      res.render("celebrities/celebrities", { celebrities });
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+app.post("/celebrities/create", (req, res, next) => {
+  const { name, occupation, catchPhrase } = req.body;
+
+  Celebrity.create({
+    name,
+    occupation,
+    catchPhrase,
+  })
+    .then((celebrity) => {
+      console.log(celebrity);
+      res.redirect("/");
+    })
+    .catch((error) => {
+      next(error);
+    });
 });
 
 app.use((error, req, res, next) => {
   console.log(error);
-  res.render('error');
+  res.render("error");
 });
 
 mongoose
@@ -28,6 +59,6 @@ mongoose
     app.listen(process.env.PORT);
   })
   .catch((error) => {
-    console.log('There was an error connecting to the database');
+    console.log("There was an error connecting to the database");
     console.log(error);
   });
--- models/celebrity.js
+++ models/celebrity.js
@@ -1,1 +1,18 @@
 // Your first model here
+const mongoose = require("mongoose");
+
+//Instantiating new Schema
+const celebritySchema = new mongoose.Schema({
+  name: String,
+  occupation: String,
+  catchPhrase: {
+    type: String,
+    required: true,
+  },
+});
+
+//Create a new model
+const Celebrity = mongoose.model("Celebrity", celebritySchema);
+
+// Export model
+module.exports = Celebrity;
